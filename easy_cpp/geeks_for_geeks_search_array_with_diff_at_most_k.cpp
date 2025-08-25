4ef68a6ffe10a8492cb2c6aea45be98c6cdd841e
Jay Shorey <129683180+itsjayshorey@users.noreply.github.com>
2025-08-25 14:28:22 +0530

    up

---
+++ easy_cpp/geeks_for_geeks_search_array_with_diff_at_most_k.cpp
@@ -0,0 +1,20 @@
+// x is the element to be searched in arr[0..n-1]
+// such that all elements differ by at-most k.
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class Solution {
+  public:
+    int findStepKeyIndex(vector<int>& arr, int k, int x) {
+        // code here
+        for(int i = 0 ; i<arr.size() ; i++)
+        {
+            if(arr[i] == x)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
