9448616b3246965262b790d930e64883baaa2f2d
Jay Shorey <129683180+itsjayshorey@users.noreply.github.com>
2025-08-30 21:52:19 +0530

    up

--- easy_cpp/geeks_for_geeks_revers_using_stack.cpp
+++ easy_cpp/geeks_for_geeks_revers_using_stack.cpp
@@ -1,15 +1,43 @@
 #include<iostream>
-using namespace std;    
+// using namespace std;    
+// class Solution {
+//   public:
+//     string reverse(const string& S) {
+//         // code here
+//         string ans = "";
+//         string s2 = S;
+//         while(!s2.empty())
+//         {
+//             ans.push_back(s2.back());
+//             s2.erase(s2.end()-1);
+//         }
+//         return ans;
+//     }
+// };
+#include<stack>
+using namespace std;
 class Solution {
   public:
     string reverse(const string& S) {
         // code here
-        string ans = "";
-        string s2 = S;
-        while(!s2.empty())
+        // string ans = "";
+        // string s2 = S;
+        // while(!s2.empty())
+        // {
+        //     ans.push_back(s2.back());
+        //     s2.erase(s2.end()-1);
+        // }
+        // return ans;
+        stack<char> stk;
+        for(int i = 0;i<S.size();i++)
         {
-            ans.push_back(s2.back());
-            s2.erase(s2.end()-1);
+            stk.push(S[i]);
+        }
+        string ans = "";
+        while(!stk.empty())
+        {
+            ans.push_back(stk.top());
+            stk.pop();
         }
         return ans;
     }
