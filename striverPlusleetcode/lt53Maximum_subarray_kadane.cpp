c85911d6568e4f1d6d95c07d0974483370042908
Jay Shorey <129683180+itsjayshorey@users.noreply.github.com>
2025-09-28 15:40:56 +0530

    kadane lt53
    lt53

---
+++ striverPlusleetcode/lt53Maximum_subarray_kadane.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <climits>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr = 0;
+        int maxi = INT_MIN;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            curr+=nums[i];
+            if(maxi<curr)
+            {
+                maxi = curr;
+            }
+            if(curr<0)
+            {
+                curr=0;
+            }
+        }
+        return maxi;
+    }
+};
